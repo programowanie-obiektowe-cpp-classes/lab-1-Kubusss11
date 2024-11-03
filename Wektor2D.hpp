c74dc90dce5adf7a133249cff3807de15925c2aa
Kubusss11 <kuba808x@interia.pl>
2024-11-03 23:24:28 +0100

    lab-1-Kubusss11 Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,29 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+private:
+    double x;
+    double y;
+public:
+    Wektor2D() //konstruktor domyslny
+    {
+        x = 0;
+        y = 0;
+    }
+    Wektor2D(double xcord_1, double xcord_2) // konstruktor ustawiajacy wspolrzedne
+    {
+        x = xcord_1;
+        y = xcord_2;
+    }
+    double getX() { return x; } //gettery
+    double getY() { return y; }
+    void setX(double xcord_1) { x = xcord_1; } //settery
+    void setY(double xcord_2) { y = xcord_2; }
 };
+Wektor2D operator+(Wektor2D v1, Wektor2D v2) // operator dodawania
+{
+    return Wektor2D(v1.getX() + v2.getX(), v1.getY() + v2.getY());
+}
+double operator*(Wektor2D v1, Wektor2D v2) //iloczyn skalarny
+{
+    return v1.getX() * v2.getX() + (v1.getY() * v2.getY()); 
+}
